a7883a1795d3d62a0426ce173b78f4397af2f22f
hvalmer <hvalmer@gmail.com>
2020-06-11 15:01:22 -0300

    Conectando ao MongoDB com repository e service

--- src/main/java/com/braincustom/workshopmongo/resources/UserResource.java
+++ src/main/java/com/braincustom/workshopmongo/resources/UserResource.java
@@ -1,27 +1,27 @@
 package com.braincustom.workshopmongo.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.braincustom.workshopmongo.domain.User;
+import com.braincustom.workshopmongo.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+	@Autowired
+	private UserService service;
+	
 	@RequestMapping(method=RequestMethod.GET)
 	public ResponseEntity<List<User>> findAll(){
-		User harlan = new User("1", "Harlan Valmer", "hvalmer@gmail.com");
-		User vanessa = new User("2", "Vanessa Lima", "nessal1979@gmail.com");
-		User meg = new User("3", "Meg", "meg@miau.com");
-		List<User> list = new ArrayList<>();
-		list.addAll(Arrays.asList(harlan, vanessa, meg));
+		
+		List<User> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 }
